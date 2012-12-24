b4edb929b6e871be856c2c7bf91f5f62e5804876
TheLRBH <bibouh123@live.fr>
2012-12-24 18:43:26 +0100

    add documentation example

--- MyFriend/includes.php
+++ MyFriend/includes.php
@@ -4,6 +4,5 @@ include_once 'admin/Connection.php';
 include_once 'admin/OwerUser.php';
 include_once 'help/Alert.php';
 
-define('ROOT_DIRE', 'file:///C:/EasyPHP-12.1/www/MyFriendWebService/MyFriend/');
 Connection::getDbMapper("myfriend");
 ?>
